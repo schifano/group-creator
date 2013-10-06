b319e41630d7b3310fc7787bf74eb9cfa5dd076c
jdboom <jdboomg@ilstu.edu>
2013-10-06 09:23:33 -0500

    adding next version of sorting class
    This should be all the classes we need. The sorting class will not quite
    compile yet.

--- GroupProgram1/src/ilstu/edu/Group.java
+++ GroupProgram1/src/ilstu/edu/Group.java
@@ -1,4 +1,5 @@
 package edu.ilstu;
+
 /*
  * Filename: Group.java
  * Oct 3, 2013
@@ -11,55 +12,79 @@ import java.util.ArrayList;
 
 /**
  * @author John Boomgarden
- *
- * This class contains an arrayList of Student Objects.
- *
+ * 
+ *         This class contains an arrayList of Student Objects.
+ * 
  */
-public class Group {
+public class Group extends ArrayList<Student> {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private ArrayList<Student> studentList;
+
 	/**
-	 *   Generic constructor which creates initialized array;
+	 * Generic constructor which creates initialized array;
 	 */
-	public Group() 
-	{
+	public Group() {
 		studentList = new ArrayList<Student>();
-	
+
 	}
+
 	/**
-	 *   This method will add a Student object to the arrayList
+	 * This method will add a Student object to the arrayList
 	 */
-	public void addStudent(Student stu1)
-	{
+	public void addStudent(Student stu1) {
 		studentList.add(stu1);
 	}
-	
+
 	/**
-	 *   This method will return the number of Students contained in the group
-	 *   @return number of Students in the Group
+	 * This method will return the number of Students contained in the group
+	 * 
+	 * @return number of Students in the Group
 	 */
-	public int getGroupsize()
-	{
-	 	int groupSize = 0;
-	 	groupSize = studentList.size();
-	 	return groupSize;
-	}	
-	
+	public int getGroupSize() {
+		int groupSize = 0;
+		groupSize = studentList.size();
+		return groupSize;
+	}
+
 	/**
-	 *   This method will call the Student method to see if the passed in Student matches
-	 *   any of the students already contained in the arrayList contained in this Group. 
-	 *   @return boolean indicator which is true if the student exists in the Group
+	 * This method will call the Student method to see if the passed in Student
+	 * matches any of the students already contained in the arrayList contained
+	 * in this Group.
+	 * 
+	 * @return boolean indicator which is true if the student exists in the
+	 *         Group
 	 */
-	public boolean containsStudent(Student stu1)
-	{
+	public boolean containsStudent(Student stu1) {
 		boolean inGroup;
 		inGroup = false;
-		for(int i = 0; i < studentList.size();i ++)
-		{
+		for (int i = 0; i < studentList.size(); i++) {
 			if (studentList.get(i).checkStudent(stu1))
 				inGroup = true;
 		}
-		
+
 		return inGroup;
+
+	}
+
+	public int getNumberFemalesInGroup() {
+		int groupNumFemales = 0;
+		for (int i = 0; i < this.getGroupSize(); i++) {
+			if (this.get(i).getGender() == "F")
+				groupNumFemales++;
+		}
+		return groupNumFemales;
+	}
+
+	public int getNumberMalesInGroup() {
+		int groupNumMales = 0;
+		for (int i = 0; i < this.getGroupSize(); i++) {
+			if (this.get(i).getGender() == "M")
+				groupNumMales++;
+		}
+		return groupNumMales;
 	}
 }
