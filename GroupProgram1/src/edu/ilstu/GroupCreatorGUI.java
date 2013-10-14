ad645f6c6142af6dc6ddaa799b4ebb8381ebc5a3
Rachel Schifano <rschifa1@gmail.com>
2013-10-14 15:36:40 -0500

    Completed commenting GroupCreatorGUI.java methods.

--- GroupProgram1/src/edu/ilstu/GroupCreatorGUI.java
+++ GroupProgram1/src/edu/ilstu/GroupCreatorGUI.java
@@ -4,23 +4,24 @@ import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
-import java.io.IOException;
 
 import javax.swing.JFrame;
 import javax.swing.JButton;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JFileChooser;
 
 /**
  * Description: GroupCreatorGUI that generates the GUI that allows a user to
- * select the various ways of creating a group.
+ * select the various ways of sorting a group or optionally upload a previous
+ * group file to compare the new group file to. This GUI provides a Create
+ * Group button that will launch the GroupListGUI where the new group file
+ * will appear.
  * @author Rachel A Schifano
  */
 
 public class GroupCreatorGUI extends JFrame {
 	
-	// Create new Sort Panel object
+	// instance variables
 	private StudentOptionsGUI studentOptions;
 	private GroupCreatorSortPanel groupCreatorSortPanel;
 	private UploadPanel uploadPanel;
@@ -28,14 +29,13 @@ public class GroupCreatorGUI extends JFrame {
 	private GroupListApp groupListApp;
 	private GroupListGUI groupListGUI;
 	
-	// Create button panel and buttons
+	// create button panel and buttons
 	private JPanel buttonPanel;
 	private JButton createGroupButton;
 	private JButton backButton;
 	private JButton cancelButton;
 	
-	// Create new file chooser object
-	private JFileChooser fc;
+	// create objects for file choosing
 	private File currentFile;
 	
 	/**
@@ -43,14 +43,13 @@ public class GroupCreatorGUI extends JFrame {
 	 */
 	public GroupCreatorGUI(File studentOptionFile) {
 		
-		
 		// Display title
 		super("Lab Group Creator Application");
 		
 		//Specify an action for the close button
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		// stores the filepath of the file being operated on
+		// stores the file path of the file being operated on
 		currentFile = studentOptionFile;
 		
 		groupListApp = new GroupListApp();
@@ -74,27 +73,30 @@ public class GroupCreatorGUI extends JFrame {
 		setVisible(true);
 		
 	}
-	
 
 	/**
-	 * The buildButtonPanel method builds a button panel.
+	 * The buildButtonPanel method builds a button panel
+	 * with a create group and back button.
 	 */
 	private void buildButtonPanel() 
 	{
-		
+		// create new button panel
 		buttonPanel = new JPanel();
 		
+		// create new create group button
+		// add action listener for launching the new group list
 		createGroupButton = new JButton("Create Group");
 		createGroupButton.addActionListener(new CreateGroupButtonListener());
 		
+		// create new back button
+		// add action listener for the user go back to the previous GUI
 		backButton = new JButton("Back");
 		backButton.addActionListener(new BackButtonListener());
 		
+		// add buttons to the panel
 		buttonPanel.add(backButton);
 		buttonPanel.add(createGroupButton);
 	}
-	
-
 	
 	/**
 	 * Private inner class that handles the event when
@@ -104,12 +106,16 @@ public class GroupCreatorGUI extends JFrame {
 	{
 		public void actionPerformed(ActionEvent e)
 		{
+			// Determine if the input for the number of groups entered or group size
+			// is equal to 0. Display an error message. 
 			if(groupListApp.getNumGroups() == 0 && groupListApp.getGroupSize() == 0)
 			{
 				JOptionPane.showMessageDialog(null, "Please Select a Sort Option", "Error", JOptionPane.ERROR_MESSAGE);
 			}
 			else
 			{
+				// Pass the current group student file and the optional uploaded previous group list file
+				// Store the newly generated student list and pass the final group list into the GroupListGUI
 				finalGroupList = groupListApp.CreateGroupList(currentFile, uploadPanel.getGroupPreviousFile());
 				groupListGUI = new GroupListGUI(finalGroupList);
 			}
@@ -117,32 +123,17 @@ public class GroupCreatorGUI extends JFrame {
 	      }
 	}
 	
-	
-	// May be needed per Corbin's request
 	/**
-	 * Private inner class that handles the event when
-	 * the user clicks on the back button
+	 * Private inner class that handles the event when the user clicks on the back button
 	 */
 	private class BackButtonListener implements ActionListener {
 		public void actionPerformed(ActionEvent e)
 		{
+			// disposes current frame
 			dispose();
+			// passes current student list file to the StudentOptionsGUI 
+			// to take the user back to the previous StudentOptions frame
 			studentOptions = new StudentOptionsGUI(currentFile);
 		}
 	}
-	
-	
-	
-	
-	/**
-	 * Private inner class that handles the event when
-	 * the user clicks on the cancel button
-	 */
-	private class CancelButtonListener implements ActionListener {
-		public void actionPerformed(ActionEvent e)
-		{
-			// Exit the application
-			System.exit(0);
-		}
-	}	
 }
